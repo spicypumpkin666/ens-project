df3dd7cd5a69208a952cf384f74614ba0ed0f6f9
spicypumpkin666 <spicypumpkin666@proton.me>
2022-08-11 13:49:15 +0200

    query opensea for asset metadata

--- get_assets.py
+++ get_assets.py
@@ -1,9 +1,70 @@
 import requests
+import time
+import json
 
 import config as cfg
 
 
-def get_assets():
-	r = requests.get(cfg.base_url+"/assets") #, auth=(cfg.user, cfg.pw))
+ens_names = {'frank.eth': {}, '123456.eth': {}, 'test0.eth': {}}
 
-	return r.text
+
+"""
+1. retrieve assets - address = owner_id
+2. retrieve asset - address = asset_contract[address]/token_id
+"""
+
+def get_token_address(ns, ens_name):
+	print(ns)
+	return ns.address(ens_name)
+
+
+def get_assets_opensea(address):
+	url = "https://testnets-api.opensea.io/api/v1/asset/"+address
+
+	response = requests.get(url)
+
+	return json.loads(response.text)
+
+
+def get_asset_metadata(ns):
+	average_last_sale = 0
+
+	for name in ens_names:
+		print(name)
+		address = get_token_address(ns, name)
+		ens_names[name]['address'] = address
+
+		url_owner_assets = "https://testnets-api.opensea.io/api/v1/assets?owner=" + address
+
+		time.sleep(10)
+
+		response = requests.get(url_owner_assets)
+
+		owner_assets_data = json.loads(response.text)
+
+
+		if owner_assets_data["assets"]:
+
+
+			token_id = owner_assets_data["assets"][0]["token_id"]
+			asset_contract_address = owner_assets_data["assets"][0]["asset_contract"]["address"]
+
+			ens_names[name]['token_id'] = token_id
+			ens_names[name]['asset_adress'] = asset_contract_address
+
+			asset_url = f"https://testnets-api.opensea.io/api/v1/asset/{asset_contract_address}/{token_id}/"
+
+			res = requests.get(asset_url)
+			asset_data = json.loads(res.text)
+
+			if asset_data.get("last_sale", None):
+
+				average_last_sale+=asset_data["last_sale"]
+
+		else:
+			print(response)
+			print(data)
+
+	print(average_last_sale/len(ens_names))
+
+	return average_last_sale/len(ens_names)
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import requests
+import json
 import web3
 
 from web3 import Web3, HTTPProvider
 from web3.middleware import geth_poa_middleware
 from ens import ENS
 
-from get_assets import get_assets
-from get_token_address import get_tokens
-
+from get_assets import get_asset_metadata
 
 def main():
 	print("beep")
@@ -21,20 +20,7 @@ def main():
 
 	ns = ENS.fromWeb3(w3)
 
-	# get address
-	address = ns.address('hermes.eth')
-
-
-	url = "https://testnets-api.opensea.io/api/v1/asset_contract/"+str(address)
-
-	response = requests.get(url)
-
-	print(response)
-
-	print(response.text)
-
-
-	# print(get_tokens(ns))
+	get_asset_metadata(ns)
 
 
 
